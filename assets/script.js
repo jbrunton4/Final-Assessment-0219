b5b9adb0bdba55d777b8b99a78a8b719e2193fbb
Josh Brunton <39703189+jbrunton4@users.noreply.github.com>
2019-02-26 07:32:51 +0000

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+function easter() {
+    alert('Easter is never on the same day on two consecutive years.');
+    console.log('Completed easter() successfully');
+}
+function christmas() {
+    alert('Christmas is the celebration of the birth of Jesus Christ.')
+    console.log('Completed christmas() successfully')
+}
+function valentinesDay() {
+    alert('Valentines day is on 14th February.')
+    console.log('Completed valentinesDay() successfully')
+}
+function go() {
+    alert("I couldn't think of another holiday.")
+    console.log('Completed go() successfully.')
+}
